c77fd993efc7d0c96a7e2d5734bb4bc753f3ee7a
Pilon <pillukv@dunwoody.edu>
2018-05-08 22:41:26 -0500

    integrating react components into existing html and css from phase 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,19 +8,19 @@ class Footer extends Component {
                 <p>Luke Pilon, Angie Toner, Will Pellegrene</p>
                 <p>Sources:</p>
                 <div id="sources">
-                    <div class="source">
+                    <div className="source">
                     <a href="https://www.fas.usda.gov/data/coffee-world-markets-and-trade">USDA Coffee Trade Data</a>
                     </div>
-                    <div class="source">
+                    <div className="source">
                     <a href="https://www.olympiacoffee.com/">Olympia Coffee</a>
                     </div>
-                    <div class="source">
+                    <div className="source">
                     <a href="https://www.intelligentsiacoffee.com/learn-do/community/intelligentsia-direct-trade">Intelligentsia Coffee</a>
                     </div>
-                    <div class="source">
+                    <div className="source">
                     <a href="http://www.bbc.com/future/story/20171106-the-disease-that-could-change-how-we-drink-coffee">BBC Article on Coffee Rust</a>
                     </div>
-                    <div class="source">
+                    <div className="source">
                     <a href="https://driftaway.coffee/arabica-robusta/">Driftaway Coffee</a>
                     </div>
                 </div>
